159f10809606f90911800cb7d9758c6cee7145c2
Creator_Boi <98004496+AnanyoBhattacharya@users.noreply.github.com>
2022-06-03 15:48:57 +0530

    This is a Random Number Generator
    *Does not include CSS. Very basic code!

---
+++ script.js
@@ -0,0 +1,6 @@
+function generateRandomNumber() {
+  var min = parseInt(document.getElementById("min").value);
+  var max = parseInt(document.getElementById("max").value);
+  var random = Math.floor(Math.random() * (max - min + 1)) + min;
+  document.getElementById("display").innerText = random;
+}
